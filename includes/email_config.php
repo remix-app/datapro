dc12c485cce49e569fe4b773e2734ba5c7380272
AppRemix <free.remixes.4u@gmail.com>
2026-01-16 08:23:59 -0300

    Update SMTP configuration details

--- includes/email_config.php
+++ includes/email_config.php
@@ -2,11 +2,11 @@
 // Configuración de Correo Electrónico (SMTP)
 // RENOMBRAR ESTE ARCHIVO A: email_config.php y completar con los datos reales
 
-define('SMTP_HOST', 'mail.datapolis.cl'); // Tu servidor de correo (ej. mail.midominio.com o smtp.gmail.com)
-define('SMTP_USER', 'sitio-web@datapolis.cl'); // Tu correo completo
-define('SMTP_PASS', 'Ww23@hoTtoZa691asYuhJP34!'); // La contraseña del correo
+define('SMTP_HOST', 'ma.cl'); // Tu servidor de correo (ej. mail.midominio.com o smtp.gmail.com)
+define('SMTP_USER', 'sit@.cl'); // Tu correo completo
+define('SMTP_PASS', 'Ww23@!'); // La contraseña del correo
 define('SMTP_PORT', 465); // Puerto SMTP (465 para SSL, 587 para TLS)
 define('SMTP_SECURE', 'ssl'); // 'ssl' o 'tls'
 
 define('MAIL_FROM_NAME', 'Web Datapolis'); // Nombre que aparecerá en el remitente
-define('MAIL_TO_ADDRESS', 'hola@datapolis.cl'); // A dónde llegarán los correos del formulario
+define('MAIL_TO_ADDRESS', 'ho.cl'); // A dónde llegarán los correos del formulario
